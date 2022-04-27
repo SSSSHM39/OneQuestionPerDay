40bc7f1a4dd4ff7adf40fe72f69a89b11e9c5dc8
SSSSHM 39 <s1986847600@gmail.com>
2022-04-27 13:23:49 +0800

    添加注释

--- AcWing-95-费解的开关.cpp
+++ AcWing-95-费解的开关.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 using namespace std;
 const int N = 6;
-char g[N][N], backup[N][N];
-int dx[5] = {-1, 0, 1, 0, 0}, dy[5] = {0, 1, 0, -1, 0};
-void turn(int x, int y)
+char g[N][N], backup[N][N];                             //记录开光状态，backup做备份
+int dx[5] = {-1, 0, 1, 0, 0}, dy[5] = {0, 1, 0, -1, 0}; //位移
+void turn(int x, int y)                                 //改变开关状态
 {
     for (int i = 0; i < 5; i++)
     {
@@ -21,21 +21,21 @@ int main()
     cin >> n;
     while (n--)
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++) //输入开关状态
             cin >> g[i];
-        int res = 7;
-        for (int o = 0; o < 32; o++)
+        int res = 7;                 //记录改变次数，大于6输出-1
+        for (int o = 0; o < 32; o++) //改变第一行开关状态，有2^5（32）种改变方式
         {
-            memcpy(backup, g, sizeof g);
-            int step = 0;
+            memcpy(backup, g, sizeof g); //拷贝备份
+            int step = 0;                //记录真实改变次数
             for (int i = 0; i < 5; i++)
-                if (o >> i & 1)
+                if (o >> i & 1) //通过二进制位运算来判断是否改变当前开关
                 {
                     step++;
                     turn(0, i);
                 }
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 4; i++) //根据上一行的开关状态来判断是否改变当前行开关
             {
                 for (int j = 0; j < 5; j++)
                 {
@@ -49,7 +49,7 @@ int main()
                 }
             }
             bool dark = false;
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++) //根据题目性质，只需判断最后一行开关是否全亮即可判断所有开关
             {
                 if (g[4][i] == '0')
                 {
@@ -59,7 +59,7 @@ int main()
             }
             if (!dark)
                 res = min(step, res);
-            memcpy(g, backup, sizeof g);
+            memcpy(g, backup, sizeof g); //恢复现场
         }
         if (res > 6)
             res = -1;
